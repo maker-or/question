f1a8f2812fa8678b019cfd41013cc2e1055f5f55
Harshith Pasupuleti <harshithpasupuleti@example.com>
2025-03-19 18:28:03 +0530

    posthog bug fixed

--- src/app/_analytics/providers.tsx
+++ src/app/_analytics/providers.tsx
@@ -1,38 +1,69 @@
-"use client";
-import { useAuth, useUser } from "@clerk/nextjs";
-import posthog from "posthog-js";
-import { PostHogProvider } from "posthog-js/react";
-import { useEffect } from "react";
+// "use client";
 
-if (typeof window !== "undefined") {
-  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: "/ingest",
-    ui_host: "https://app.posthog.com",
-  });
-}
+// import posthog from "posthog-js";
+// import { PostHogProvider } from "posthog-js/react";
+// import { useEffect } from "react";
 
-export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
+// if (typeof window !== "undefined") {
+//   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+//     api_host: "/ingest",
+//     ui_host: "https://app.posthog.com",
+//   });
+// }
+
+// export function CSPostHogProvider({ children }: { children: React.ReactNode }) {
+//   return (
+//     <PostHogProvider client={posthog}>
+//       <PostHogAuthWrapper>{children}</PostHogAuthWrapper>
+//     </PostHogProvider>
+//   );
+// }
+
+// function PostHogAuthWrapper({ children }: { children: React.ReactNode }) {
+//   // const auth = useAuth();
+//   // const userInfo = useUser();
+
+//   useEffect(() => {
+//     if (userInfo.user) {
+//       posthog.identify(userInfo.user.id, {
+//         email: userInfo.user.emailAddresses[0]?.emailAddress,
+//         name: userInfo.user.fullName,
+//       });
+//     } else if (!auth.isSignedIn) {
+//       posthog.reset();
+//     }
+//   }, [auth, userInfo]);
+
+//   return children;
+// }
+
+
+// app/providers.jsx
+// app/providers.tsx
+'use client'
+
+import posthog from 'posthog-js'
+import { PostHogProvider as PHProvider } from 'posthog-js/react'
+import { useEffect } from 'react'
+import PostHogPageView from "./PostHogPageView"
+
+export function PostHogProvider({ children }: { children: React.ReactNode }) {
+    useEffect(() => {
+      const posthogKey = process.env.NEXT_PUBLIC_POSTHOG_KEY;
+      if (posthogKey) {
+        posthog.init(posthogKey, {
+          api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+          capture_pageview: false // Disable automatic pageview capture, as we capture manually
+        });
+      } else {
+        console.error('NEXT_PUBLIC_POSTHOG_KEY is not defined');
+      }
+  }, [])
+
   return (
-    <PostHogProvider client={posthog}>
-      <PostHogAuthWrapper>{children}</PostHogAuthWrapper>
-    </PostHogProvider>
-  );
-}
-
-function PostHogAuthWrapper({ children }: { children: React.ReactNode }) {
-  const auth = useAuth();
-  const userInfo = useUser();
-
-  useEffect(() => {
-    if (userInfo.user) {
-      posthog.identify(userInfo.user.id, {
-        email: userInfo.user.emailAddresses[0]?.emailAddress,
-        name: userInfo.user.fullName,
-      });
-    } else if (!auth.isSignedIn) {
-      posthog.reset();
-    }
-  }, [auth, userInfo]);
-
-  return children;
+    <PHProvider client={posthog}>
+         <PostHogPageView />
+      {children}
+    </PHProvider>
+  )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "../utils/theme-provider"
-import { CSPostHogProvider } from "../app/_analytics/providers";
+import { PostHogProvider } from "../app/_analytics/providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <CSPostHogProvider>
+    <PostHogProvider>
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <ThemeProvider
@@ -38,6 +38,6 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
-    </CSPostHogProvider>
+    </PostHogProvider>
   );
 }
